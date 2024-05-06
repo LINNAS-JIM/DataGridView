090daf4c175045ae9c05f29de925c702a85b4a12
LINNAS-JIM <168353952+LINNAS-JIM@users.noreply.github.com>
2024-05-06 21:11:32 +0600

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace abcd
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idtb = new System.Windows.Forms.TextBox();
+            this.nametb = new System.Windows.Forms.TextBox();
+            this.departmenttb = new System.Windows.Forms.TextBox();
+            this.passwordtb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idtb
+            // 
+            this.idtb.Location = new System.Drawing.Point(107, 96);
+            this.idtb.Name = "idtb";
+            this.idtb.Size = new System.Drawing.Size(100, 22);
+            this.idtb.TabIndex = 0;
+            // 
+            // nametb
+            // 
+            this.nametb.Location = new System.Drawing.Point(107, 148);
+            this.nametb.Name = "nametb";
+            this.nametb.Size = new System.Drawing.Size(100, 22);
+            this.nametb.TabIndex = 1;
+            // 
+            // departmenttb
+            // 
+            this.departmenttb.Location = new System.Drawing.Point(107, 196);
+            this.departmenttb.Name = "departmenttb";
+            this.departmenttb.Size = new System.Drawing.Size(100, 22);
+            this.departmenttb.TabIndex = 2;
+            // 
+            // passwordtb
+            // 
+            this.passwordtb.Location = new System.Drawing.Point(107, 239);
+            this.passwordtb.Name = "passwordtb";
+            this.passwordtb.Size = new System.Drawing.Size(100, 22);
+            this.passwordtb.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DEPARTMENT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PASSWORD";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(230, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(568, 319);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(387, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(500, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordtb);
+            this.Controls.Add(this.departmenttb);
+            this.Controls.Add(this.nametb);
+            this.Controls.Add(this.idtb);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idtb;
+        private System.Windows.Forms.TextBox nametb;
+        private System.Windows.Forms.TextBox departmenttb;
+        private System.Windows.Forms.TextBox passwordtb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abcd
+{
+    public partial class Form3 : Form
+    {
+        private string connection_string = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\source\repos\STUDENT MANAGEMENT SYSTEM\DATABASE\DATABASE SERVER.mdf;Integrated Security=True;Connect Timeout=30";
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connection_string;
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select * from [Table]";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = table;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string id = idtb.Text.ToString();
+            string name = nametb.Text.ToString();
+            string department = departmenttb.Text.ToString();
+            string password = passwordtb.Text.ToString();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connection_string;
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select * from [Table] where id = '" + id + "'";
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                MessageBox.Show("The id already exist.\nEnter a different valid id");
+            }
+            else
+            {
+                reader.Close();
+                SqlCommand cmd1 = new SqlCommand();
+                cmd1.Connection = conn;
+                cmd1.CommandText = "insert into [Table] (id,[student name], [department name],  passward) values (@id, @student_name, @department_name,  @passward)";
+                cmd1.Parameters.AddWithValue("@id", id);
+                cmd1.Parameters.AddWithValue("@student_name", name);
+                cmd1.Parameters.AddWithValue("@department_name", department);
+                cmd1.Parameters.AddWithValue("@passward", password);
+                cmd1.ExecuteNonQuery();
+                MessageBox.Show("Data Inserted");
+            }
+
+
+            conn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+
+            idtb.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            nametb.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            departmenttb.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            passwordtb.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string id = idtb.Text.ToString();
+            string name = nametb.Text.ToString();
+            string department = departmenttb.Text.ToString();
+            string password = passwordtb.Text.ToString();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connection_string;
+            conn.Open();
+            
+           
+            
+            
+                
+            SqlCommand cmd1 = new SqlCommand();
+            cmd1.Connection = conn;
+            cmd1.CommandText = "update [Table] set [student name] = @student_name, [department name] = @department_name,  passward = @passward where id = " +id;
+            cmd1.Parameters.AddWithValue("@id", id);
+            cmd1.Parameters.AddWithValue("@student_name", name);
+            cmd1.Parameters.AddWithValue("@department_name",department );
+            cmd1.Parameters.AddWithValue("@passward", password);
+            cmd1.ExecuteNonQuery();
+            MessageBox.Show("Row Updated");
+            
+
+
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string id = idtb.Text.ToString();
+            string name = nametb.Text.ToString();
+            string department = departmenttb.Text.ToString();
+            string password = passwordtb.Text.ToString();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connection_string;
+            conn.Open();
+
+            SqlCommand cmd1 = new SqlCommand();
+            cmd1.Connection = conn;
+            cmd1.CommandText = "delete from [Table] where id = " + id;
+            cmd1.ExecuteNonQuery();
+            MessageBox.Show("Row Deleted");
+
+            conn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
